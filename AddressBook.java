5491d3200d9d07431fa9011b35a339305920e735
vaishnavi birle <vaishnavibirle1133@gmail.com>
2021-04-29 00:04:37 +0530

    Created Contact

--- AddressBook.java
+++ AddressBook.java
@@ -1,9 +1,43 @@
 package com.company;
 
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Scanner;
+
 public class AddressBook {
 
+    static Scanner sc = new Scanner(System.in);
+    static List<Contact> list = new LinkedList<Contact>();
+
+    //Created method for adding contact
+    public static void addContact()
+    {
+        System.out.println("Enter your firstName : ");
+        String firstName = sc.nextLine();
+        System.out.println("Enter your lastName : ");
+        String lastName = sc.nextLine();
+        System.out.println("Enter your address : ");
+        String address = sc.nextLine();
+        System.out.println("Enter your city : ");
+        String city = sc.nextLine();
+        System.out.println("Enter your state : ");
+        String state = sc.nextLine();
+        System.out.println("Enter your zipCode : ");
+        String zip = sc.nextLine();
+        System.out.println("Enter your phoneNo : ");
+        String phoneNo = sc.nextLine();
+        System.out.println("Enter your emailId : ");
+        String email = sc.nextLine();
+        Contact obj = new Contact(firstName, lastName, address, city, state, zip, phoneNo, email);
+        list.add(obj);
+    }
+
+
     public static void main(String[] args) {
-	// write your code here
+        //Displaying the welcome message
         System.out.println("WELCOME TO ADDRESS BOOK PROBLEM");
+        //Creating contact
+        addContact();
+        System.out.println(list); //printing list
     }
 }
